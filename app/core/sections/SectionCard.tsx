a7bb9d7d328d8083a4ba8bd026d4a9e880806d4e
Andrés Chacón <achaconm2001@gmail.com>
2021-06-17 17:25:26 -0600

    Secciones terminadas

---
+++ app/core/sections/SectionCard.tsx
@@ -0,0 +1,49 @@
+import { Box, Flex, Image, Text, Heading } from "@chakra-ui/react"
+import { FC } from "react"
+import { BiTimeFive } from "react-icons/bi"
+
+type Props = {
+  photo: string
+  tag: string
+  title: string
+  description: string
+  helper: string
+  duration: string
+}
+
+export const SectionCard: FC<Props> = (props) => {
+  return (
+    <Box backgroundColor="white" boxShadow="base" rounded="md" overflow="hidden">
+      <Flex borderColor="gray.200" direction="column" overflowWrap="break-word">
+        <Image src={props.photo} alt="Foto" h="60" objectFit="cover" />
+        <Box px="6" py="5">
+          <Text
+            textTransform="uppercase"
+            letterSpacing="0.05em"
+            fontSize="xs"
+            fontWeight="600"
+            mb="2"
+            color="gray.500"
+          >
+            {props.tag}
+          </Text>
+          <Heading fontWeight="700" fontSize="md" lineHeight="base" mb="2">
+            {props.title}
+          </Heading>
+          <Text isTruncated noOfLines={2} mb="8" color="gray.600">
+            {props.description}
+          </Text>
+          <Flex justifyContent="space-between" alignItems="center" fontSize="sm" color="gray.600">
+            <Text>{props.helper}</Text>
+            <Flex alignItems="center">
+              <BiTimeFive size={17} />
+              <Text ml="2">{props.duration}</Text>
+            </Flex>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
+
+export default SectionCard
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,11 +1,35 @@
 import { BlitzPage } from "blitz"
 import Layout from "app/core/layouts/Layout"
 import Section from "app/core/sections/Section"
+import SectionCard from "app/core/sections/SectionCard"
 
 const Home: BlitzPage = () => {
   return (
     <Section title="Nuevas Sesiones" footerText="Ver todas las sesiones">
-      xd
+      <SectionCard
+        photo="https://images.unsplash.com/photo-1544367567-0f2fcb009e0b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1094&q=80"
+        tag="Yoga"
+        title="Yoga para todas las edad sin importar la condición física."
+        description="Nuestra nueva clase de yoga te ayudará a mejorar tu postura y arreglará todos esos años de dolores de espalda!"
+        helper="San Jose Centro"
+        duration="2 horas y media"
+      />
+      <SectionCard
+        photo="https://images.unsplash.com/photo-1517838277536-f5f99be501cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+        tag="Pesa"
+        title="Pesas para fortalecimiento de muñeca y biceps."
+        description="La rutina 2 veces a la semana para fortalecer los biceps y mejorar el agarre de la muñeca!"
+        helper="San Jose Centro"
+        duration="2 horas y media"
+      />
+      <SectionCard
+        photo="https://images.unsplash.com/photo-1530143584546-02191bc84eb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+        tag="Bicicleta"
+        title="Horas de bicicleta con vista al bosque y ruidos de fondo."
+        description="La bicicleta es de los mejores ejercicios para el fortalecimiento de pantorrilla junto con una playlist de música relajante."
+        helper="San Jose Centro"
+        duration="2 horas y media"
+      />
     </Section>
   )
 }
