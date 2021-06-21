ba175ab807c6d8adfdc3bcc86c3fa270cbb890e0
Andrés Chacón <achaconm2001@gmail.com>
2021-06-21 14:39:51 -0600

    Reacomodo y limpieza de código

--- app/room-sessions/pages/sessions/index.tsx
+++ app/room-sessions/pages/sessions/index.tsx
@@ -1,38 +1,13 @@
 import React, { Suspense } from "react"
-import { Head, Link, usePaginatedQuery, useRouter, BlitzPage, Routes } from "blitz"
+import { Head, BlitzPage } from "blitz"
 import Layout from "app/core/layouts/Layout"
-import getRoomSessions from "app/room-sessions/queries/getRoomSessions"
-import SectionCard from "app/core/sections/SectionCard"
-import { Box } from "@chakra-ui/layout"
 import Section from "app/core/sections/Section"
+import RoomSessionAll from "app/room-sessions/components/RoomSessionAll"
 
 export const RoomSessionsList = () => {
-  const router = useRouter()
-  const [{ roomSessions }] = usePaginatedQuery(getRoomSessions, {})
-
   return (
     <Section title="Sesiones">
-      {roomSessions.map((roomSession) => (
-        <Link
-          key={roomSession.id}
-          href={Routes.ShowRoomSessionPage({ roomSessionId: roomSession.id })}
-        >
-          <a>
-            <SectionCard
-              photo={roomSession.room.photo}
-              tag={roomSession.specialities.join(", ")}
-              title={`Sala: ${roomSession.room.name}`}
-              description={`Tenemos una capacidad de ${
-                roomSession.room.maxCapacity
-              } pero estamos aceptando solo ${
-                roomSession.room.maxCapacityAllowed
-              }. Además cubrimos las especialidades de ${roomSession.specialities.join(", ")}`}
-              helper={`Administrador por ${roomSession.instructor.name}`}
-              duration={`${roomSession.scheduleId}`}
-            />
-          </a>
-        </Link>
-      ))}
+      <RoomSessionAll />
     </Section>
   )
 }
