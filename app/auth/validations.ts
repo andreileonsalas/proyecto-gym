53d416c567f5a12b39543be9e0392c1d7b054995
Andrés Chacón <achaconm2001@gmail.com>
2021-06-20 19:00:27 -0600

    Cuentas con foto

--- app/auth/validations.ts
+++ app/auth/validations.ts
@@ -5,6 +5,7 @@ const password = z.string().min(10, "Ingrese mínimo 10 caracteres.").max(100)
 export const Signup = z.object({
   name: z.string(),
   email: z.string().email("El email es inválido."),
+  photo: z.string(),
   password,
 })
 
